8165612ef79bcba364075114470365e970594eae
faridanang <f4r1danangs@gmail.com>
2024-07-29 12:45:30 +0800

    commit

---
+++ popup.js
@@ -0,0 +1,65 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("accountList").addEventListener("click", changeAccount)
+    document.getElementById("userAddress").addEventListener("click", copyAddress)
+    document.getElementById("transferFund").addEventListener("click", handler)
+})
+
+// State Variable
+let providerUrl = '';
+
+let provider;
+let privateKey;
+let address;
+
+// Function
+function handler() { };
+
+function checkBalance() { };
+
+function getOpenNetwork() { };
+
+function getSelectedNetwork() { };
+
+function setNetwork() { };
+
+function loginUser() { };
+
+function createUser() { };
+
+function openCreate() { };
+
+function signUp() { };
+
+function logIn() { };
+
+function logout() { };
+
+function openTransfer() { };
+
+function goBack() { };
+
+function openImport() { };
+
+function importGoBack() { }
+
+function openActivity() { };
+
+function openAssets() { };
+
+function goHomePage() { };
+
+function openImportModel() { };
+
+function closeImportModel() { };
+
+function addToken() { };
+
+
+function addAccount() { };
+
+function myFunction() { };
+
+function copyAddress() { };
+
+function changeAccount() { };
+
